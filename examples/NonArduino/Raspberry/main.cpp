84305c599e77fbd053570fc0302809774e6abbf7
SocioPJ <sociopj45@outlook.com>
2024-03-04 19:00:28 -0300

    SX1278

--- examples/NonArduino/Raspberry/main.cpp
+++ examples/NonArduino/Raspberry/main.cpp
@@ -1,17 +1,3 @@
-/*
-   RadioLib Non-Arduino Raspberry Pi Example
-
-   This example shows how to use RadioLib without Arduino.
-   In this case, a Raspberry Pi with WaveShare SX1302 LoRaWAN Hat
-   using the pigpio library.
-
-   Can be used as a starting point to port RadioLib to any platform!
-   See this API reference page for details on the RadioLib hardware abstraction
-   https://jgromes.github.io/RadioLib/class_hal.html
-
-   For full API reference, see the GitHub Pages
-   https://jgromes.github.io/RadioLib/
-*/
 
 // include the library
 #include <RadioLib.h>
@@ -26,16 +12,16 @@ PiHal* hal = new PiHal(1);
 
 // now we can create the radio module
 // pinout corresponds to the Waveshare LoRaWAN Hat
-// NSS pin:   7
-// DIO1 pin:  17
-// NRST pin:  22
-// BUSY pin:  not connected
-SX1261 radio = new Module(hal, 7, 17, 22, RADIOLIB_NC);
+// NSS pin:   8 RASP(8)
+// DIO0 pin:  17 RASP(17)
+// DIO1 pin:  13 RASP(13)
+// RST pin:  12 RASP(4)
+SX1278 radio = new Module(hal, 8, 17, 4, 13);
 
 // the entry point for the program
 int main(int argc, char** argv) {
   // initialize just like with Arduino
-  printf("[SX1261] Initializing ... ");
+  printf("[SX1278] Initializing ... ");
   int state = radio.begin();
   if (state != RADIOLIB_ERR_NONE) {
     printf("failed, code %d\n", state);
@@ -46,7 +32,7 @@ int main(int argc, char** argv) {
   // loop forever
   for(;;) {
     // send a packet
-    printf("[SX1261] Transmitting packet ... ");
+    printf("[SX1278] Transmitting packet ... ");
     state = radio.transmit("Hello World!");
     if(state == RADIOLIB_ERR_NONE) {
       // the packet was successfully transmitted
