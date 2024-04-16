94bc18bb45cddb534850e28094da9aaa50ee20e0
SocioPJ <sociopj45@outlook.com>
2024-04-16 11:22:23 -0300

    Working but it can be improved!

--- examples/NonArduino/Raspberry/main.cpp
+++ examples/NonArduino/Raspberry/main.cpp
@@ -1,10 +1,15 @@
-#include <csignal> // Biblioteca para lidar com sinais
+#include "PiHal.h"
 #include <iostream>
 #include <RadioLib.h>
-#include "PiHal.h"
+#include <csignal> // Biblioteca para lidar com sinais
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <cstdint>
 
 bool keepRunning = true;
 
@@ -30,98 +35,117 @@ SX1276 radio = new Module(hal, 8, 17, 4, 13);
 AFSKClient audio(&radio, 12);
 SSTVClient sstv(&radio);
 
-uint32_t line[320] = {
-  // black
-  0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000,
-  0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000, 0x000000,
-
-  // blue
-  0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF,
-  0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF, 0x0000FF,
-
-  // green
-  0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00,
-  0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00, 0x00FF00,
-
-  // cyan
-  0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF,
-  0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF, 0x00FFFF,
-
-  // red
-  0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000,
-  0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000, 0xFF0000,
-
-  // magenta
-  0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF,
-  0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF, 0xFF00FF,
-
-  // yellow
-  0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00,
-  0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00, 0xFFFF00,
-
-  // white
-  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF,
-  0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF, 0xFFFFFF
-};
-
 int main(int argc, char **argv)
 {
-  FILE *fptr;
-  uint32_t teste[320];
-  if ((fptr = fopen("/home/tccsat/RadioLib/examples/NonArduino/Raspberry/cat.txt","r")) == NULL){
-       printf("Error! opening file");
-       exit(1);
-   }
-  fscanf(fptr,"%x,",&teste[0]);
-  fclose(fptr);
-  printf("%x",teste[0]);
-  // uint32_t *line = image[1];
-  // printf("i%",line);
-  // for (int i=0; i<256;i++){
-  
-  // }
+  std::vector<std::vector<uint32_t>> image;
+  std::ifstream file("/home/tccsat/RadioLib/examples/NonArduino/Raspberry/cat.txt");
+  if (file.is_open())
+  {
+    for (int i = 0; i < 256; i++)
+    {
+      std::string line;
+      std::getline(file, line);
+      std::vector<uint32_t> lista;
+      std::istringstream iss(line);
+      std::string token;
+      while (std::getline(iss, token, ','))
+      {
+        uint32_t value = std::stoi(token, nullptr, 16);
+        lista.push_back(value);
+      }
+      if (lista.size() != 320)
+      {
+        std::cerr << "O arquivo não contém 320 valores na linha " << (i + 1) << std::endl;
+        return 1;
+      }
+      image.push_back(lista);
+      std::cout << "Tamanho da linha " << (i + 1) << ": " << lista.size() << std::endl;
+    }
+  }
+  else
+  {
+    std::cerr << "Não foi possível abrir o arquivo." << std::endl;
+    return 1;
+  }
+  std::cout << "Tamanho da imagem: " << image.size() << std::endl;
+
+  if (!image.empty())
+  {
+    std::cout << "Primeira linha da imagem:" << std::endl;
+    for (uint32_t pixel : image[0])
+    {
+      std::cout << std::hex << pixel << " ";
+    }
+    std::cout << std::endl;
+  }
+  else
+  {
+    std::cerr << "A imagem está vazia." << std::endl;
+  }
+
   int state = radio.beginFSK();
-  if (state == RADIOLIB_ERR_NONE){
+  if (state == RADIOLIB_ERR_NONE)
+  {
     printf("success!\n");
-  } else {
+  }
+  else
+  {
     printf("failed, code %i\n", state);
   }
 
   printf("Inicializando SSTV!\n");
-  state = sstv.begin(915.0 ,Martin2);
-  if (state == RADIOLIB_ERR_NONE){
+  state = sstv.begin(915.0, Martin2);
+  if (state == RADIOLIB_ERR_NONE)
+  {
     printf("success sstv!\n");
-  } else {
+  }
+  else
+  {
     printf("failed, code %i\n", state);
   }
 
   printf("Setting correction ...");
   state = sstv.setCorrection(1.0);
-  if (state == RADIOLIB_ERR_NONE){
+  if (state == RADIOLIB_ERR_NONE)
+  {
     printf("success correction!\n");
-  } else {
+  }
+  else
+  {
     printf("failed, code %i\n", state);
   }
-    signal(SIGINT, sigHandler);
-  while(keepRunning){
+  signal(SIGINT, sigHandler);
 
+
+
+  
+  while (keepRunning)
+  {
+    uint32_t line[320];
     printf("Sending test picture ...");
-    hal->delay(1000);
+    hal->delay(100);
     sstv.sendHeader();
-    for (uint32_t i = 0; i < sstv.getPictureHeight(); i++){
-      if (keepRunning){
-      sstv.sendLine(line);
-      } else {
+    for (uint32_t j = 0; j < sstv.getPictureHeight(); j++)
+    {
+      for (size_t i = 0; i < 320; ++i)
+      {
+        line[i] = image[j][i];
+      }
+      if (keepRunning)
+      {
+        sstv.sendLine(line);
+      }
+      else
+      {
         radio.reset();
         break;
       }
     }
-
     radio.standby();
-
     printf("Done!");
     hal->delay(1000);
-
   }
-  return (0);
+  file.close();
+
+  return 0;
 }
