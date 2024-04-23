53004a95188cb0167a63d328ca61044de70a0ce9
SocioPJ <sociopj45@outlook.com>
2024-04-23 09:15:43 -0300

    endnode

--- examples/NonArduino/Raspberry/endnode.cpp
+++ examples/NonArduino/Raspberry/endnode.cpp
@@ -1,10 +1,10 @@
 
 // include the library
 #include <RadioLib.h>
-#inclde <string>
+#include <string>
 // include the hardware abstraction layer
 #include "PiHal.h"
-#define loRaPort = 2000;
+// #define loRaPort = 100;
 // create a new instance of the HAL class
 // use SPI channel 1, because on Waveshare LoRaWAN Hat,
 // the SX1261 CS is connected to CE1
@@ -22,7 +22,7 @@ LoRaWANNode node(&radio, &US915, 2);
 // the entry point for the program
 int main(int argc, char** argv) {
 
-  printf("[SX1272 - LoRa] Initializing ... ");
+  printf("[SX1276 - LoRa] Initializing ... ");
   int state = radio.begin();
   if (state != RADIOLIB_ERR_NONE) {
     printf("failed, code %d\n", state);
@@ -32,37 +32,38 @@ int main(int argc, char** argv) {
   }
 
 
-  uint64_t joinEUI = 0x1111111111111111;
-  uint64_t devEUI = 0x20007043200100443;
+  uint32_t devAddr = 0x20010443;
   uint8_t nwkKey[] = { 0x74, 0x6F, 0x70, 0x53, 0x65, 0x63, 0x72, 0x65,
                        0x74, 0x4B, 0x65, 0x79, 0x31, 0x33, 0x33, 0x34 };
 
   uint8_t appKey[] = { 0x61, 0x44, 0x69, 0x66, 0x66, 0x65, 0x72, 0x65,
                        0x6E, 0x74, 0x4B, 0x65, 0x79, 0x41, 0x42, 0x43 };
   printf("[LoRaWAN] Attempting over the air activation ...");
-  state = node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+  state = node.beginABP(devAddr, nwkKey, appKey);
 
   if (state != RADIOLIB_ERR_NONE){
     printf("failed, code %d\n", state);
     return(1);
   } else {
     printf("success!\n");
-    hal->dela(2000)
+    hal->delay(2000);
   }
   int count = 0;
   
-  for(;;) {
+  while(true) {
     // send a packet
     printf("[LoRaWAN] Sending uplink packet ... ");
-    string strUp = "Hello!" + std::to_string(loRaPort);
-    string strDown;
-    int state = node.sendReceive(strUp, loRaPort, strDown);
+    const char* strUp = "Hello!";
+    uint8_t port = 100;
+    uint8_t dataDown[256];
+    size_t lenDown = 0;
+    int state = node.sendReceive(strUp,port,dataDown,&lenDown);
     if(state == RADIOLIB_ERR_NONE) {
       // the packet was successfully transmitted
       printf("Received a downlink!\n");
 
       // wait for a second before transmitting again
-      hal->delay(100);
+      hal->delay(1000);
 
     } else {
       printf("failed, code %d\n", state);
