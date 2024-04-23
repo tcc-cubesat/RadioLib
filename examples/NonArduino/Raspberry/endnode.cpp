b5b9bf5dcfe4dfc94447b2209aca6b55af081627
SocioPJ <sociopj45@outlook.com>
2024-04-23 09:47:27 -0300

    received on networkserver

--- examples/NonArduino/Raspberry/endnode.cpp
+++ examples/NonArduino/Raspberry/endnode.cpp
@@ -8,7 +8,7 @@
 // create a new instance of the HAL class
 // use SPI channel 1, because on Waveshare LoRaWAN Hat,
 // the SX1261 CS is connected to CE1
-PiHal* hal = new PiHal(1);
+PiHal *hal = new PiHal(1);
 
 // now we can create the radio module
 // pinout corresponds to the Waveshare LoRaWAN Hat
@@ -18,59 +18,63 @@ PiHal* hal = new PiHal(1);
 // RST pin:  12 RASP(4)
 SX1276 radio = new Module(hal, 8, 17, 4, 13);
 
-LoRaWANNode node(&radio, &US915, 2);
+LoRaWANNode node(&radio, &AU915, 2);
 // the entry point for the program
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
   printf("[SX1276 - LoRa] Initializing ... ");
   int state = radio.begin();
-  if (state != RADIOLIB_ERR_NONE) {
+  if (state != RADIOLIB_ERR_NONE)
+  {
     printf("failed, code %d\n", state);
-    return(1);
-  }else {
+    return (1);
+  }
+  else
+  {
     printf("success!\n");
   }
 
-
   uint32_t devAddr = 0x20010443;
-  uint8_t nwkKey[] = { 0x74, 0x6F, 0x70, 0x53, 0x65, 0x63, 0x72, 0x65,
-                       0x74, 0x4B, 0x65, 0x79, 0x31, 0x33, 0x33, 0x34 };
-
-  uint8_t appKey[] = { 0x61, 0x44, 0x69, 0x66, 0x66, 0x65, 0x72, 0x65,
-                       0x6E, 0x74, 0x4B, 0x65, 0x79, 0x41, 0x42, 0x43 };
+  uint8_t nwkKey[] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C};
+  uint8_t appKey[] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C};
   printf("[LoRaWAN] Attempting over the air activation ...");
   state = node.beginABP(devAddr, nwkKey, appKey);
 
-  if (state != RADIOLIB_ERR_NONE){
+  if (state != RADIOLIB_ERR_NONE)
+  {
     printf("failed, code %d\n", state);
-    return(1);
-  } else {
+    return (1);
+  }
+  else
+  {
     printf("success!\n");
     hal->delay(2000);
   }
   int count = 0;
-  
-  while(true) {
+
+  while (true)
+  {
     // send a packet
     printf("[LoRaWAN] Sending uplink packet ... ");
-    const char* strUp = "Hello!";
+    const char *strUp = "Hello!";
     uint8_t port = 100;
     uint8_t dataDown[256];
     size_t lenDown = 0;
-    int state = node.sendReceive(strUp,port,dataDown,&lenDown);
-    if(state == RADIOLIB_ERR_NONE) {
+    int state = node.sendReceive(strUp, port, dataDown, &lenDown);
+    if (state == RADIOLIB_ERR_NONE)
+    {
       // the packet was successfully transmitted
       printf("Received a downlink!\n");
 
       // wait for a second before transmitting again
-      hal->delay(1000);
-
-    } else {
-      printf("failed, code %d\n", state);
-
+      hal->delay(10000);
     }
-
+    else
+    {
+      printf("failed, code %d\n", state);
+    }
   }
 
-  return(0);
+  return (0);
 }
